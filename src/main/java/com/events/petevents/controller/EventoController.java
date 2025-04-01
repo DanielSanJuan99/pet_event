2065b0dcc6fce6a402f3d2f13e691ebf6edbe3b8
Daniel San Juan <dsanjuan1999@gmail.com>
2025-03-31 21:48:42 -0300

    Implementación de manejo de errores

---
+++ src/main/java/com/events/petevents/controller/EventoController.java
@@ -0,0 +1,34 @@
+package com.events.petevents.controller;
+
+import com.events.petevents.model.Evento;
+import com.events.petevents.service.EventoService;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/eventos")
+public class EventoController {
+    
+    private final EventoService eventoService;
+
+    public EventoController(EventoService eventoService) {
+        this.eventoService = eventoService;
+    }
+
+    @GetMapping
+    public List<Evento> getAllEvents() {
+        return eventoService.getAllEvents();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Evento> getEventoById(@PathVariable Long id) {
+        return eventoService.getEventoById(id);
+    }
+}
---
+++ src/main/java/com/events/petevents/exception/EventoNotFound.java
@@ -0,0 +1,11 @@
+package com.events.petevents.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EventoNotFound extends RuntimeException {
+    public EventoNotFound(Long id) {
+        super("Evento ("+ id +") no encontrado");
+    }
+}
